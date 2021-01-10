846499f0169d792c32c2735b0ac5a3309250552b
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-10 17:28:53 +0400

    solved first problem

--- src/CollectNewspaperKarel.java
+++ src/CollectNewspaperKarel.java
@@ -7,7 +7,34 @@ import stanford.karel.*;
 public class CollectNewspaperKarel extends SuperKarel {
 	
 	public void run() {
-	    
+		pick();
+		comeBack();
     }
+
+    private void pick() {
+		while(frontIsClear()) {
+			move();
+		}
+		turnRight();
+		while (leftIsBlocked()) {
+			move();
+		}
+		turnLeft();
+		move();
+		pickBeeper();
+		turnAround();
+	}
+	private void comeBack() {
+		while(frontIsClear()) {
+			move();
+		}
+		turnRight();
+		while (frontIsClear()){
+			move();
+		}
+		turnRight();
+	}
+
+
 
 }
