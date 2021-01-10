923f07e722e4f9e571486b357faecd9a03859add
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-10 19:33:43 +0400

    solved second problem

--- src/StoneMasonKarel.java
+++ src/StoneMasonKarel.java
@@ -6,6 +6,47 @@ import stanford.karel.*;
 
 public class StoneMasonKarel extends SuperKarel {
 	
-	// TODO: write the code to implement this program
+	public void run() {
+        while (frontIsClear()) {
+            repairColumn();
+            moveToNextColumn();
+        }
+        if (frontIsBlocked()) {
+            repairColumn();
+        }
+
+
+    }
+
+
+    private void repairColumn() {
+        turnLeft();
+        while (frontIsClear()) {
+            replaceStones();
+            move();
+
+        }
+        replaceStones();
+        turnAround();
+        while (frontIsClear()) {
+            move();
+        }
+        turnLeft();
+    }
+
+    private void moveToNextColumn() {
+	    for (int i = 0; i<4; i++) {
+	        move();
+        }
+    }
+
+    private void replaceStones() {
+	    if (beepersPresent()) {
+
+        } else {
+	        putBeeper();
+
+        }
+    }
 
 }
