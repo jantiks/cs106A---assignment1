c3332e0b42e3700302e67f661c2e49e26f6676c1
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-11 20:34:24 +0400

    solved third assignment for all words besides 8x1 world

--- src/CheckerboardKarel.java
+++ src/CheckerboardKarel.java
@@ -5,7 +5,55 @@
 import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
-	
-	// TODO: write the code to implement this program
+	int row = 0;
+	public void run() {
+
+        while (frontIsClear() || row == 0) {
+
+            putBeepersInOneRow();
+            changeLine();
+        }
+
+        row = 0;
+
+    }
+
+    private void putBeepersInOneRow() {
+        putBeeper();
+        while(frontIsClear()) {
+
+            move();
+            if (frontIsClear()) {
+                move();
+                putBeeper();
+            }
+
+        }
+
+    }
+
+    private void changeLine() {
+
+	    if (row%2==0) {
+            turnLeft();
+            if (frontIsClear()) {
+                move();
+                turnLeft();
+            }
+
+            row++;
+        } else {
+            turnRight();
+            if (frontIsClear()) {
+                move();
+                turnRight();
+            }
+            row++;
+        }
+
+    }
+
+
+
 
 }
