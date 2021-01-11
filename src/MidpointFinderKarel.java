fd74caea3710085f703f700f1da9d4cad44c6f3a
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-12 00:30:43 +0400

    assignment 4 is done

--- src/MidpointFinderKarel.java
+++ src/MidpointFinderKarel.java
@@ -6,6 +6,24 @@ import stanford.karel.*;
 
 public class MidpointFinderKarel extends SuperKarel {
 	
-	// TODO: write the code to implement this program
+	public void run() {
+
+	    for (int i = 1; i < 100; i++) {
+	        move();
+	        if (frontIsBlocked()) {
+	            turnAround();
+	            for (int j = 1; j <= i/2; j++) {
+	                move();
+	            }
+	            putBeeper();
+	            if (i%2==1) {
+	                move();
+	                putBeeper();
+	            }
+	            break;
+	        }
+	    }
+
+    }
 
 }
